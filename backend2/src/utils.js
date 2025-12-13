23da2b421822bb23fc1c79a7e6c16e762273e27e
Ehud Guzman <nyamuehud@gmail.com>
2025-12-13 11:24:32 +0300

    Netlify build fix for React 18 lightbox

--- backend2/src/utils.js
+++ backend2/src/utils.js
@@ -2,12 +2,13 @@ import fs from "fs";
 import path from "path";
 
 export const readJSON = (filename) => {
-  const filePath = path.join(process.cwd(), "src", "data", filename);
+  // Changed from "src/data" to just "data"
+  const filePath = path.join(process.cwd(), "data", filename);
   const raw = fs.readFileSync(filePath);
   return JSON.parse(raw);
 };
 
 export const writeJSON = (filename, data) => {
-  const filePath = path.join(process.cwd(), "src", "data", filename);
+  const filePath = path.join(process.cwd(), "data", filename);
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 };
